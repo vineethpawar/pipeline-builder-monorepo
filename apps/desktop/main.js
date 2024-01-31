e486aa33b530f11ad0e42b100ac66480628111bb
vineethpawar <vineethpawar99@gmail.com>
2024-02-01 02:55:46 +0530

    chore: wip desktop file system setup

--- apps/desktop/main.js
+++ apps/desktop/main.js
@@ -1,30 +1,82 @@
-const { app, BrowserWindow } = require('electron/main')
-const path = require('node:path')
+const path = require('path')
+const url = require('url')
+const { app, BrowserWindow } = require('electron')
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+let mainWindow
 
-  win.loadFile('index.html')
+let isDev = false
+
+if (
+	process.env.NODE_ENV !== undefined &&
+	process.env.NODE_ENV === 'development'
+) {
+	isDev = true
 }
 
-app.whenReady().then(() => {
-  createWindow()
+function createMainWindow() {
+	mainWindow = new BrowserWindow({
+		width: 1100,
+		height: 800,
+		show: false,
+		icon: `${__dirname}/assets/icon.png`,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	})
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+	let indexPath
+
+	if (isDev && process.argv.indexOf('--noDevServer') === -1) {
+		indexPath = url.format({
+			protocol: 'http:',
+			host: 'localhost:8080',
+			pathname: 'index.html',
+			slashes: true,
+		})
+	} else {
+		indexPath = url.format({
+			protocol: 'file:',
+			pathname: path.join(__dirname, 'dist', 'index.html'),
+			slashes: true,
+		})
+	}
+
+	mainWindow.loadURL(indexPath)
+
+	// Don't show until we are ready and loaded
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show()
+
+		// Open devtools if dev
+		if (false) {
+			const {
+				default: installExtension,
+				REACT_DEVELOPER_TOOLS,
+			} = require('electron-devtools-installer')
+
+			installExtension(REACT_DEVELOPER_TOOLS).catch((err) =>
+				console.log('Error loading React DevTools: ', err)
+			)
+			mainWindow.webContents.openDevTools()
+		}
+	})
+
+	mainWindow.on('closed', () => (mainWindow = null))
+}
+
+app.on('ready', createMainWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
+
+app.on('activate', () => {
+	if (mainWindow === null) {
+		createMainWindow()
+	}
+})
+
+// Stop error
+app.allowRendererProcessReuse = true
---
+++ apps/desktop/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+	return (
+		<div className='app' style={{background:'red'}}>
+			<h1>React Electron Boilerplate</h1>
+			<p>This is a simple boilerplate for using React with Electron</p>
+		</div>
+	)
+}
+
+export default App
