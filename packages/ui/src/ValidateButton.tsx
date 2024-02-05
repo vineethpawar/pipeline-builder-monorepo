2db06bb9c7dc4b915984944f4ddfd3207b3b2db6
vineethpawar <vineethpawar99@gmail.com>
2024-02-05 14:40:38 +0530

    chore: disable validate CTA when no node

--- packages/ui/src/ValidateButton.tsx
+++ packages/ui/src/ValidateButton.tsx
@@ -1,11 +1,14 @@
 import { CheckSquareOutlined } from '@ant-design/icons'
 import { Button } from 'antd'
 import React from 'react'
+import { useMyContext } from './MyContext'
 
 const ValidateButton = () => {
+  const {nodes} = useMyContext();
   return (
     <Button
         type="primary" 
+        disabled={nodes.length === 0}
         size='large'
         style={{ position: "absolute", bottom: "20px", right: "20px", zIndex: 100, backgroundColor:'green'}}
         onClick={()=>{}}
